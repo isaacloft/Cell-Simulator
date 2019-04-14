9872055479c11626865a7541661392484e4d53c7
isaac <isaac.guan@rxpservices.com>
2019-04-14 15:58:36 +1000

    Add more fun to the cell; Use chainable bem modifier to define scss.

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,12 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ICell from '../interfaces/cell-type';
 
 const Cell = (props: ICell) => {
   const { id, cellOnClickHandler, isActivated } = props;
+  const [cellColorModifier, setCellColorModifier] = useState('');
 
-  useEffect(() => {}, [isActivated]);
+  // By using this useEffect, the cellColorModifier is only called at the first time
+  useEffect(() => {
+    setCellColorModifier(randomColorfulCell());
+  }, []);
 
-  const cellClassName = `cell-container ${isActivated ? 'isActivated' : 'isDeactivated'}`;
+  const randomColorfulCell = () => {
+    const arrayOfColors = ['-green-cell', '-yellow-cell', '-blue-cell', '-brown-cell', '-black-cell'];
+    const maximum = arrayOfColors.length;
+    const minimum = 0;
+    const randomIndex = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+    return arrayOfColors[randomIndex];
+  };
+
+  // const = randomColorfulCell();
+  const cellClassName = `game-pane__cell-container ${
+    isActivated ? '-isActivated' : '-isDeactivated'
+  } ${cellColorModifier}`;
+
   return <div id={id} className={cellClassName} onClick={cellOnClickHandler} />;
 };
 
